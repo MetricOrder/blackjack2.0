c79b31ff14d4389183acff1f078296b650faf3ae
MetricOrder <106072188+MetricOrder@users.noreply.github.com>
2023-06-19 19:59:34 +1000

    put card elements into an array

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let firstCard = 10
 let secondCard = 4
 let drawCard = 6
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,12 +10,12 @@ let messageEl = document.querySelector("#message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
-function startGame(){
+function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + ", " + cards[1]
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
